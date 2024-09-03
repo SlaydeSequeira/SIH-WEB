3d53c3fcba15474a4d93f818cf95826e59708451
SlaydeSequeira <crce.9638.ce@gmail.com>
2024-09-03 17:42:00 +0530

    360 vr works

---
+++ a.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+struct Stack1
+{
+  int a[10];
+  int top;
+};
+int main()
+{
+  Stack1* s;
+  s->top=0;
+  s->a[s->top]=1;
+  cout<<s->a[s->top];
+    return 0;
+}
